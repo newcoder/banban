cc3cc765211f7dd595d766aba6fb27d00e9a670c
ray.ni <ray.ni@outlook.com>
2016-07-15 12:47:11 +0800

    add set up deep learning environment notes

--- plot.py
+++ plot.py
@@ -1,7 +1,17 @@
-import pylab
 import numpy as np
-from scipy.stats import norm
-x = np.linspace(-50,50,1000)
-y = norm.pdf(x, loc=-1.55, scale=8.66)    # for example
-pylab.plot(x,y)
-pylab.show()
+import matplotlib.pyplot as plt
+
+mu, sigma = 100, 15
+x = mu + sigma * np.random.randn(10000)
+
+# the histogram of the data
+n, bins, patches = plt.hist(x, 50, normed=1, facecolor='g', alpha=0.75)
+
+
+plt.xlabel('Smarts')
+plt.ylabel('Probability')
+plt.title('Histogram of IQ')
+plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
+plt.axis([40, 160, 0, 0.03])
+plt.grid(True)
+plt.show()
